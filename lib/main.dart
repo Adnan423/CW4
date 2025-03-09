148565427340fc7e7c05643d7435e78aa38710f0
Adnan Khan <jackmarkiplier@gmail.com>
2025-03-09 19:19:37 -0400

    first 3 classes for the plan and the code for managing it

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class Plan {
+  String name;
+  String description;
+  DateTime date;
+  bool isCompleted;
+
+  Plan({required this.name, required this.description, required this.date, this.isCompleted = false});
+}
+
+class PlanManagerScreen extends StatefulWidget {
+  @override
+  _PlanManagerScreenState createState() => _PlanManagerScreenState();
+}
+
+class _PlanManagerScreenState extends State<PlanManagerScreen> {
+  List<Plan> plans = [];
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDate = DateTime.now();
